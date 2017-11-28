e9d5eac78414e9493be92d7e25b91ddc0dfa61f7
zsy93 <sydzhang@ucdavis.edu>
2017-11-27 22:58:54 -0800

    Add files via upload

--- env/envs.py
+++ env/envs.py
@@ -1,8 +1,9 @@
 import os, sys
 import math
 import numpy as np
-from pray_policy import Pray
+
 sys.path.insert(1,os.path.join(sys.path[0],'..'))
+from pray_policy import Pray
 
 from multiagent.environment import MultiAgentEnv
 import multiagent.scenarios as scenarios
@@ -37,29 +38,39 @@ class Environ:
         self.env.render()
 
     def step(self,act_n):
-        agent_actions = self.action_transfer(act_n)
+        agent_actions = self.action_transfer(act_n,self.current_obs)
         pray_action = self.pray.action(self.current_obs)
         actions = np.vstack((agent_actions,pray_action))
+
+        #print(actions)
         obs_n, reward_n, done_n, _ = self.env.step(actions)
         agents_obs = obs_n[0][:self.num_agents, :]
         self.current_obs = obs_n[0]
-        return agents_obs, np.squeeze(reward_n[:self.num_agents]), done_n[0]
+        done = False
+        for ii in range(self.num_agents):
+            if done_n[ii]:
+                done = True
+        return agents_obs, np.squeeze(reward_n[:self.num_agents]), done
 
     def reset(self):
-        self.env.reset()
+        return self.env.reset()[0]
 
 
-    def action_transfer(self, action):
+    def action_transfer(self, action, current_obs):
+        max_edge=0.4
+        u = np.zeros((self.num_agents, 5))
         for i in range(self.num_agents):
-            u = np.zeros((self.num_agents, 5))
+
             movable = action[i, 0]
             angle = action[i, 1]
 
-            if movable <= 0:
+            if movable <= -1:
                 u[i,0] += 1
-            if movable > 0:
+            if movable > -1:
                 direction_x = math.cos(angle * math.pi)
                 direction_y = math.sin(angle * math.pi)
+                x=current_obs[i,0]
+                y = current_obs[i, 1]
                 if direction_x > 0:
                     u[i, 1] += 1
                 if direction_x < 0:
@@ -68,4 +79,13 @@ class Environ:
                     u[i, 3] += 1
                 if direction_y < 0:
                     u[i, 4] += 1
+                if u[i,1] > 0 and x >= max_edge:
+                    u[i,1] = 0
+                if u[i,2] > 0 and x <= -max_edge:
+                    u[i,2] = 0
+                if u[i,3] > 0 and y >= max_edge:
+                    u[i,3] = 0
+                if u[i,4] > 0 and y <= -max_edge:
+                    u[i,4] = 0
+
         return u
