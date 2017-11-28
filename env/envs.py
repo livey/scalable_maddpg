fa8a9e72c431efd8ce2f17abc5b5415f616533af
livey <liveysmile@gmail.com>
2017-11-27 16:01:12 -0800

    create new environment class

---
+++ env/envs.py
@@ -0,0 +1,80 @@
+import os, sys
+sys.path.insert(1,os.path.join(sys.path[0],'..'))
+
+from multiagent.environment import MultiAgentEnv
+from multiagent.new_policy import InteractivePolicy
+import multiagent.scenarios as scenarios
+
+
+class Environ:
+    def __init__(self,num_agents):
+        self.num_agents = num_agents
+        self.env = self.create_env(num_agents)
+
+
+
+    def create_env(self,num_agents):
+        scenario = scenarios.load('ev.py')
+        world = scenarios.make_world()
+        env = MultiAgentEnv(world)
+        return env
+
+    def render(self):
+        self.env.render()
+
+    def step(self,act_n):
+        obs_n, reward_n, done_n, _ = env.step(act_n)
+        return obs_n, reward_n, done_n
+
+    def reset(self):
+        self.env.reset()
+
+    def make_new_env(self,num_agents):
+        self.num_agents = num_agents
+        self.env = self.create_env(num_agents)
+
+if __name__ == '__main__':
+    # parse arguments
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument('-s', '--scenario', default='ev.py', help='Path of the scenario Python script.')
+    args = parser.parse_args()
+
+    # load scenario from script
+    scenario = scenarios.load(args.scenario).Scenario(3)
+    # create world
+    world = scenario.make_world()
+    # create multiagent environment
+    env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, info_callback=None, shared_viewer = False)
+    # render call to create viewer window (necessary only for interactive policies)
+    env.render()
+    # create interactive policies for each agent
+    policies = [InteractivePolicy(env,i) for i in range(env.n)]
+    # execution loop
+    obs_n = env.reset()
+    while True:
+        # query for action from each agent's policy
+        act_n = []
+        observation=obs_n[0]
+        length=observation.shape[1]
+        for i,policy in enumerate(policies):
+            if i<length-1:
+                obs=observation[i,:]
+                act_n.append(policy.action(obs))
+            if i==length-1:
+                act_n.append(policy.action(observation))
+        # step environment
+        obs_n, reward_n, done_n, _ = env.step(act_n)
+
+        # render all agent views
+        env.render()
+
+        #print action
+        print(act_n)
+        #get observation/state
+        env_obs = obs_n[0]
+        print(env_obs)
+        #get reward
+        env_reward=[]
+        for agent in env.world.agents:
+            env_reward.append(env._get_reward(agent))
+        print(env_reward)
