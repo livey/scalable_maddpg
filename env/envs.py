169ee5b81d096c24727a7861d7a54df3d1616c61
zsy93 <sydzhang@ucdavis.edu>
2017-11-29 17:07:47 -0800

    Add files via upload

--- env/envs.py
+++ env/envs.py
@@ -9,6 +9,8 @@ from multiagent.environment import MultiAgentEnv
 import multiagent.scenarios as scenarios
 
 
+max_edge=0.2
+
 class Environ:
     def __init__(self,num_agents):
         self.num_agents = num_agents
@@ -48,7 +50,10 @@ class Environ:
         self.current_obs = obs_n[0]
         done = False
         for ii in range(self.num_agents):
-            if reward_n[ii]>=8:
+            x_dis=self.current_obs[ii,0]-self.current_obs[ii,2]
+            y_dis=self.current_obs[ii,1]-self.current_obs[ii,3]
+            dis=np.sqrt(np.square(x_dis)+np.square(y_dis))
+            if dis<=0.075+0.05:
                 done= True
         return agents_obs, np.squeeze(reward_n[:self.num_agents]), done
 
@@ -57,35 +62,35 @@ class Environ:
 
 
     def action_transfer(self, action, current_obs):
-        max_edge=1
-        u = np.zeros((self.num_agents, 4))
-        for i in range(self.num_agents):
 
-            #movable = action[i, 0]
+        u = np.zeros((self.num_agents, 5))
+        for i in range(self.num_agents):
+            movable = 1
             #angle = action[i, 1]
-            angle=action
-            #if movable <= -1:
-            #    u[i,0] += 1
-            #if movable > -1:
-            direction_x = math.cos(angle * math.pi)
-            direction_y = math.sin(angle * math.pi)
-            x=current_obs[i,0]
-            y = current_obs[i, 1]
-            if direction_x > 0:
-                u[i, 0] += 1
-            if direction_x < 0:
-                u[i, 1] += 1
-            if direction_y > 0:
-                u[i, 2] += 1
-            if direction_y < 0:
-                u[i, 3] += 1
-            if u[i,0] > 0 and x >= max_edge:
-                u[i,0] = 0
-            if u[i,1] > 0 and x <= -max_edge:
-                u[i,1] = 0
-            if u[i,2] > 0 and y >= max_edge:
-                u[i,2] = 0
-            if u[i,3] > 0 and y <= -max_edge:
-                u[i,3] = 0
+            angle=action[i]
+            if movable <= -1:
+                u[i,0] += 1
+            if movable > -1:
+                direction_x = math.cos(angle * math.pi)
+                direction_y = math.sin(angle * math.pi)
+                x=current_obs[i,0]
+                y = current_obs[i, 1]
+
+                if direction_x > 0:
+                    u[i, 1] += direction_x
+                if direction_x < 0:
+                    u[i, 2] += -direction_x
+                if direction_y > 0:
+                    u[i, 3] += direction_y
+                if direction_y < 0:
+                    u[i, 4] += -direction_y
+                if u[i,1] > 0 and x >= max_edge:
+                    u[i,1] = 0
+                if u[i,2] > 0 and x <= -max_edge:
+                    u[i,2] = 0
+                if u[i,3] > 0 and y >= max_edge:
+                    u[i,3] = 0
+                if u[i,4] > 0 and y <= -max_edge:
+                    u[i,4] = 0
 
         return u
