19606bbca54dcf79d94188bf627b1447cfa6e215
zsy93 <sydzhang@ucdavis.edu>
2017-11-29 17:06:01 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from env import envs
 from maddpg import MaDDPG
 
 state_dim = 4
-action_dim = 2
+action_dim = 1
 
 num_agents = 4
 maddpg = MaDDPG(num_agents,state_dim, action_dim)
@@ -14,23 +14,26 @@ maddpg = MaDDPG(num_agents,state_dim, action_dim)
 Env = envs.Environ(num_agents)
 obs = Env.reset()
 current_state = obs
-max_time = 100000
+max_time = 1000000
 #print(current_state)
 for epoch in range(max_time):
-    print('epoch',epoch)
+    #print('epoch',epoch)
     action = maddpg.noise_action(current_state)
     #print(action)
     next_state, reward, done = Env.step(action)
     #print(reward)
     maddpg.perceive(current_state,action,reward,next_state,done)
     current_state = next_state
+    Env.render()
     if done:
         print('done')
+        print('epoch', epoch)
+
         Env.re_create_env(num_agents)
         current_state = Env.reset()
 
     #if epoch % 1000==1 or epoch% 1000 == 1 or epoch%1000==2 or epoch%1000==3:
-    #Env.render()
+    #    Env.render()
 
 
 
