4d8cbaa5f85e419250a64799eb93043618bd0562
livey <liveysmile@gmail.com>
2017-12-03 14:12:07 -0800

    with episode structure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import sys
 sys.path.insert(1,'env/')
 from env import envs
@@ -8,44 +8,58 @@ from maddpg import MaDDPG
 
 state_dim = 5
 action_dim = 1
-max_edge=0.2
+max_edge= 0.3
 
-num_agents = 3
+num_agents = 1
 maddpg = MaDDPG(num_agents,state_dim, action_dim)
 
 Env = envs.Environ(num_agents,max_edge)
 obs = Env.reset()
 current_state = obs
-max_time = 100001
-done_epoch =0
+
+max_episode = 1000000
+done_epoch = 0
 #print(current_state)
+max_epoch = 1000
+
 catch_time = []
-for epoch in range(max_time):
-    #print('epoch',epoch)
-    action = maddpg.noise_action(current_state)
-    #print(action)
-    next_state, reward, done = Env.step(action)
-    print(reward)
-    maddpg.perceive(current_state,action,reward,next_state,done)
-    #print(current_state)
-    #print(next_state)
-    pos_variant=current_state[0,:]-next_state[0,:]
-    dis=np.sqrt(np.square(pos_variant[0])+np.square(pos_variant[1]))
-    print(dis)
-    Env.render()
-    current_state = next_state
-    if done:
-        print('done at epoch: {}'.format(epoch))
-        inter = epoch - done_epoch
-        catch_time.append(inter)
-        print('episode time',  inter)
-        done_epoch = epoch
+
+for episode in range(max_episode):
+    print('episode',episode)
+    while (True):
         Env.re_create_env(num_agents)
         current_state = Env.reset()
+        action = maddpg.noise_action(current_state)
+        next_state, reward, done = Env.step(action)
+        #print(reward)
+        if not done:
+            current_state = next_state
+            break
+
+    for epoch in range(max_epoch):
+        #print('epoch',epoch)
+        #Env.render()
+        action = maddpg.noise_action(current_state)
+        #print(action)
+        next_state, reward, done = Env.step(action)
+        maddpg.perceive(current_state,action,reward,next_state,done)
+        current_state = next_state
+        if done:
+            print('Done!!!!!!!!!!!! at epoch{} , reward:{}'.format(epoch,reward))
+            inter = epoch - done_epoch
+            catch_time.append(inter)
+            # add summary for each episode
+            maddpg.summary()
+            break
+    if epoch ==max_epoch-1:
+        print('Time up >>>>>>>>>>>>>>')
+
+
+
 
     #if epoch % 1000==1 or epoch% 1000 == 1 or epoch%1000==2 or epoch%1000==3:
     #Env.render()
 
-
+maddpg.close_session()
 plt.plot(catch_time)
 plt.show()
